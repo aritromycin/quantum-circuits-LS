4f7d8c0646df374ac04087724b8814938fa93938
Jai Bellare <jaibellare@gmail.com>
2026-06-22 12:30:55 +0530

    add rotation.py with Task 1

---
+++ rotation.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# Use a single complex dtype for numpy everywhere.
+DTYPE = np.complex128
+
+INV_SQRT2 = 1.0 / np.sqrt(2.0)
+H = INV_SQRT2 * np.array([[1, 1], [1, -1]], dtype=DTYPE)
+
+
+class Bloch:
+    """Axis-angle (Bloch) form of a 2x2 unitary G:
+
+        G = e^{i alpha} (cos(theta/2) I - i sin(theta/2) (n . sigma))
+
+    i.e. a global phase e^{i alpha} times a rotation by angle `theta` about the
+    Bloch-sphere axis `n`. Here (n . sigma) = n_x X + n_y Y + n_z Z.
+    """
+
+    alpha: float  # global phase
+    n: np.ndarray  # unit rotation axis, shape (3,): [n_x, n_y, n_z]
+    theta: float  # rotation angle
+
+
+def to_bloch(g: np.ndarray) -> Bloch:
+    """Recover the Bloch form (alpha, n, theta) of a 2x2 unitary `g`.
+    """
+    raise NotImplementedError("to_bloch is not implemented yet")
